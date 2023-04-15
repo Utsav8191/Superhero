896c87836f35d3da074edba0e5eaf40348fc5c12
unknown <utsavkumargupta8191@gmail.com>
2023-04-16 00:59:47 +0530

    added pages and responsive.css

---
+++ favourites.js
@@ -0,0 +1,45 @@
+
+const favListArray = JSON.parse(localStorage.getItem('favList'));
+console.log(favListArray);
+let cardsSection = document.querySelector('section');
+
+if (favListArray.length < 1) {
+  const noResults = document.createElement('div');
+  const messageText = document.createElement('h2');
+  messageText.classList.add("messageText");
+  messageText.innerText = "Sorry Nothing In Your Favourite List !!";
+  noResults.append(messageText);
+}
+
+for (let i of favListArray) {
+ 
+  let cardContainer = document.createElement('div');  
+  cardContainer.classList.add("card-container");
+  cardsSection.append(cardContainer);
+  
+  let frontFace = document.createElement('div');
+  frontFace.classList.add("front");
+  cardContainer.append(frontFace);
+  
+  let imageTag = document.createElement('img');
+  imageTag.setAttribute("src", i.image.url ?? "");
+  frontFace.append(imageTag);
+
+  let backFace = document.createElement('div');
+  backFace.classList.add("back");
+  cardContainer.append(backFace);
+
+  let heroName = document.createElement("h2");
+  heroName.innerText = i.name;
+  backFace.append(heroName);
+
+  let deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("delete-btn");
+  backFace.append(deleteBtn);
+
+  let DeleteIcon = document.createElement("i");
+  DeleteIcon.classList.add("fa-solid");
+  DeleteIcon.classList.add("fa-trash");
+  DeleteIcon.style.color = "#363030";
+  deleteBtn.append(DeleteIcon);
+}
---
+++ superhero.js
@@ -0,0 +1,6 @@
+const sectionElement = document.querySelector("section");
+// console.log(sectionElement);
+
+const newCard = document.createElement("a")
+newCard.classList.add("card-container")
+sectionElement.append(newCard);
